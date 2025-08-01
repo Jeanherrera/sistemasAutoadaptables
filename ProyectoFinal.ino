20265664142a138fd42942aac771d728c1c271f4
Juan carlos Hurtado <34747212+Juanca14@users.noreply.github.com>
2025-07-31 21:51:09 -0500

    Ajuste proyecto

--- ProyectoFinal.ino
+++ ProyectoFinal.ino
@@ -28,13 +28,13 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 #define CNY3 40
 
 // Sensores ambientales
-#define SENSOR_PIN_S1 12  // Visibilidad
-#define SENSOR_PIN_S2 13  // Visibilidad
-#define CO2_SENSOR_PIN 14 // CO₂
+#define SENSOR_PIN_S1 13   // Visibilidad
+#define SENSOR_PIN_S2 12   // Visibilidad
+#define CO2_SENSOR_PIN 14  // CO₂
 
 // Botones semaforos
-#define BTN_PEATONAL 1 // Traffic light 1 button connected in pin 1
-#define BTN_MODE 2     // Traffic light 2 button connected in pin 2
+#define BTN_PEATONAL_S1 1  // Traffic light 1 button connected in pin 1
+#define BTN_PEATONAL_S2 2  // Traffic light 2 button connected in pin 2
 
 // Estados del semáforo
 #define RED 0
@@ -47,18 +47,22 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 int YELLOW_LED_PIN_1_STATE = LOW;
 int YELLOW_LED_PIN_2_STATE = LOW;
 
-const char *ssid = "Usuario";
-const char *password = "Clave";
+const char *ssid = "iPhone de Jean";
+const char *password = "12345679";
 
-// WebSocketsClient webSocket;
+WebSocketsClient webSocket;
 
-unsigned long previousMillis = 0;
-const unsigned long interval = 100;
+unsigned long previousMillisPriority1 = 0;
+unsigned long previousMillisPriority2 = 0;
+unsigned long previousMillisFlashing = 0;
+unsigned long previousMillisSocket = 0;
+
+const unsigned long interval = 1000;
 
 int state = 0;
 bool vlS1 = false, vlS2 = false;
 bool co2High = false;
-int sensor_other_city = 0, sensorValueS1 = 0, sensorValueS2 = 0, sensorValueCO2 = 0, veh_c1 = 0, veh_c2 = 0;
+int sensor_other_city = 0, sensorValueS1 = 0, sensorValueS2 = 0, sensorValueCO2 = 0, mode = 0;
 
 unsigned long tini, tactual, tdelta;
 bool R1 = false, Y1 = false, V1 = false;
@@ -73,24 +77,21 @@ bool lastModeBtnState = HIGH;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
 
-enum Mode
-{
+enum Mode {
   MODE_DAY,
   MODE_NIGHT
 };
 
-Mode currentMode = MODE_DAY; // Empieza por defecto en modo día
+Mode currentMode = MODE_DAY;  // Empieza por defecto en modo día
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   config_display();
   pinModes();
-  connection(); // WiFi y WebSocket
+  connection();  // WiFi y WebSocket
 }
 
-void pinModes()
-{
+void pinModes() {
 
   // Sensor CO2
   pinMode(CO2_SENSOR_PIN, INPUT);
@@ -120,52 +121,45 @@ void pinModes()
   pinMode(GREEN_LED_PIN_2, OUTPUT);
 
   // Pasos peatonales
-  pinMode(BTN_PEATONAL, INPUT_PULLUP);
-
-  // Modo
-  pinMode(BTN_MODE, INPUT_PULLUP);
+  pinMode(BTN_PEATONAL_S1, INPUT_PULLUP);
+  pinMode(BTN_PEATONAL_S2, INPUT_PULLUP);
 }
 
-void config_display()
-{
-  lcd.init();      // Inicializa la pantalla
-  lcd.backlight(); // Enciende luz de fondo
+void config_display() {
+  lcd.init();       // Inicializa la pantalla
+  lcd.backlight();  // Enciende luz de fondo
   lcd.setCursor(0, 0);
   lcd.print("Smart City Capibaras");
   lcd.clear();
 }
 
-void read_CO2_sensor()
-{
+void read_CO2_sensor() {
   sensorValueCO2 = analogRead(CO2_SENSOR_PIN);
   co2High = (sensorValueCO2 > 700);
 }
 
-void read_sensor_S1()
-{
+void read_sensor_S1() {
   sensorValueS1 = analogRead(SENSOR_PIN_S1);
   vlS1 = (sensorValueS1 < 600);
+  vlS1 = read_visibility(sensorValueS1);
 }
 
-void read_sensor_S2()
-{
+void read_sensor_S2() {
   sensorValueS2 = analogRead(SENSOR_PIN_S2);
-  vlS2 = read_visibility(sensorValueS2);
+  vlS2 = (sensorValueS2 < 600);
+  ;
 }
 
-bool read_visibility(int sensorValue)
-{
+bool read_visibility(int sensorValue) {
 
-  if (sensor_other_city != 0)
-  {
+  if (sensor_other_city != 0) {
     return (sensorValue < 600) || (sensor_other_city < 600);
   }
 
   return (sensorValue < 600);
 }
 
-void show_values_display()
-{
+void show_values_display() {
   lcd.setCursor(0, 0);
   lcd.print("Modo: ");
   lcd.print(currentMode == MODE_DAY ? "DIA" : "NOCHE");
@@ -178,17 +172,16 @@ void show_values_display()
   lcd.print(sensor_other_city);
 }
 
-void loop()
-{
+void loop() {
 
-  webSocket.loop();      // Procesar WebSocket
-  sendSignalWebsocket(); // Enviar datos cada intervalo
+  webSocket.loop();       // Procesar WebSocket
+  sendSignalWebsocket();  // Enviar datos cada intervalo
 
-  read_CO2_sensor(); // Mostrar CO₂ en LCD
-  read_sensor_S1();  // Mostrar valor de los sensores semaforo 1
-  read_sensor_S2();  // Mostrar valor de los sensores semaforo 2
-  read_lane_in_1();  // Mostrar valor de los cantidad de vehiculos en el carril 1
-  read_lane_in_2();  // Mostrar valor de los cantidad de vehiculos en el carril 2
+  read_CO2_sensor();  // Mostrar CO₂ en LCD
+  read_sensor_S1();   // Mostrar valor de los sensores semaforo 1
+  read_sensor_S2();   // Mostrar valor de los sensores semaforo 2
+  read_lane_in_1();   // Mostrar valor de los cantidad de vehiculos en el carril 1
+  read_lane_in_2();   // Mostrar valor de los cantidad de vehiculos en el carril 2
 
   // Mostras los valores de los sensores
   show_values_display();
@@ -197,7 +190,7 @@ void loop()
 
   if (currentMode == MODE_DAY)
   {
-    mode_day();
+  mode_day();
   }
   else
   {
@@ -205,18 +198,14 @@ void loop()
   }
 }
 
-void checkModeSwitch()
-{
-  int reading = digitalRead(BTN_MODE);
-  if (reading != lastModeBtnState)
-  {
+void checkModeSwitch() {
+  int reading = mode;
+  if (reading != lastModeBtnState) {
     lastDebounceTime = millis();
   }
 
-  if ((millis() - lastDebounceTime) > debounceDelay)
-  {
-    if (reading == LOW)
-    {
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading == HIGH) {
       currentMode = (currentMode == MODE_DAY) ? MODE_NIGHT : MODE_DAY;
       Serial.print("Cambiado a modo: ");
       Serial.println(currentMode == MODE_DAY ? "DIA" : "NOCHE");
@@ -226,8 +215,7 @@ void checkModeSwitch()
   lastModeBtnState = reading;
 }
 
-void mode_day()
-{
+void mode_day() {
 
   // Paso peatonal
   pedestrian_crossing();
@@ -236,91 +224,87 @@ void mode_day()
   updateLights();
 }
 
-void mode_night()
-{
+void mode_night() {
 
-  if (co2High || vlS2)
-  {
+  if (co2High || vlS1 || vlS2) {
     priorityLine2();
-  }
-  else if (veh_c1 > 0)
-  {
+  } else if (read_lane_in_1() > 0) {
     priorityLine2();
-  }
-  else if (veh_c2 > 0)
-  {
+  } else if (read_lane_in_2() > 0) {
     priorityLine1();
-  }
-  else
-  {
+  } else {
     NoVehicleMode();
   }
 }
 
-void priorityLine1()
-{
-  allOff();
+void priorityLine1() {
+
+  digitalWrite(YELLOW_LED_PIN_1, LOW);
+  digitalWrite(YELLOW_LED_PIN_2, LOW);
+
   lcd.setCursor(0, 2);
   lcd.print("Paso: Carril #1 #V:");
-  lcd.print(veh_c1);
+  lcd.print(read_lane_in_1());
 
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
+  if (currentMillis - previousMillisPriority1 >= interval) {
+    previousMillisPriority1 = currentMillis;
     digitalWrite(RED_LED_PIN_2, HIGH);
     digitalWrite(GREEN_LED_PIN_1, HIGH);
   }
 }
 
-void priorityLine2()
-{
-  allOff();
-  lcd.setCursor(0, 2);
-  lcd.print("Paso: Carril #2 #V:");
-  lcd.print(veh_c2);
+void priorityLine2() {
+
+  digitalWrite(YELLOW_LED_PIN_1, LOW);
+  digitalWrite(YELLOW_LED_PIN_2, LOW);
+
+  if (read_lane_in_2() > 0) {
+    lcd.setCursor(0, 2);
+    lcd.print("Paso: Carril #2 #V:");
+    lcd.print(read_lane_in_2());
+  }
 
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
+  if (currentMillis - previousMillisPriority2 >= interval) {
+    previousMillisPriority2 = currentMillis;
     digitalWrite(RED_LED_PIN_1, HIGH);
     digitalWrite(GREEN_LED_PIN_2, HIGH);
   }
 }
 
-void NoVehicleMode()
-{
-  allOff();
+void NoVehicleMode() {
+
+  digitalWrite(RED_LED_PIN_1, LOW);
+  digitalWrite(GREEN_LED_PIN_1, LOW);
+  digitalWrite(RED_LED_PIN_2, LOW);
+  digitalWrite(GREEN_LED_PIN_2, LOW);
+
+  yellow_flashing_both();
+
   lcd.setCursor(0, 2);
   lcd.print("Modo sin vehiculos    ");
-  yellow_flashing_both();
-  ;
 }
 
-void checkState()
-{
+void checkState() {
   tactual = millis();
   unsigned long tdelta = tactual - tini;
 
   lcd.setCursor(0, 2);
   lcd.print("Modo ");
-  lcd.print((co2High || vlS2) ? "EMERGENCIA    " : "NORMAL     ");
+  lcd.print((co2High || vlS1 || vlS2) ? "EMERGENCIA    " : "NORMAL     ");
 
   // Si se solicitó paso peatonal y no hemos cambiado aún
-  if (peatonalRequested && !peatonalReady)
-  {
-    if (millis() - peatonalRequestTime >= 3000)
-    {
+  if (peatonalRequested && !peatonalReady) {
+    if (millis() - peatonalRequestTime >= 3000) {
       peatonalReady = true;
     }
   }
 
   // Se activa el paso peatonal
-  if (peatonalReady && state != PEATONAL)
-  {
+  if (peatonalReady && state != PEATONAL) {
     changeState(PEATONAL, "Activando paso peatonal con retraso");
     peatonalRequested = false;
     peatonalReady = false;
@@ -328,121 +312,109 @@ void checkState()
   }
 
   // Si hay una condición "mala" o el CO2 es alto, y el estado lo permite, transicionar a GREEN
-  if ((co2High || vlS2) && (state == RED || state == RED_TO_YELLOW || state == GREEN_TO_YELLOW))
-  {
+  if ((co2High || vlS1 || vlS2) && (state == RED || state == RED_TO_YELLOW || state == GREEN_TO_YELLOW)) {
     state = GREEN;
     tini = millis();
     return;
   }
 
-  switch (state)
-  {
-  case RED:
-    if (tdelta >= 3000)
-    {
-      changeState(RED_TO_YELLOW, "Estado 1: Rojo a Amarillo");
-    }
-    break;
-
-  case RED_TO_YELLOW:
-    if (tdelta >= 2000)
-    {
-      changeState(GREEN, "Estado 2: Verde");
-    }
-    break;
-
-  case GREEN:
-    if (vlS2)
-    {
-      tini = millis();
+  switch (state) {
+    case RED:
+      if (tdelta >= 3000) {
+        changeState(RED_TO_YELLOW, "Estado 1: Rojo a Amarillo");
+      }
       break;
-    }
 
-    if (tdelta >= 6000 && !vlS2)
-    {
-      changeState(GREEN_TO_YELLOW, "Estado 3: Verde a Amarillo");
-    }
-    break;
+    case RED_TO_YELLOW:
+      if (tdelta >= 2000) {
+        changeState(GREEN, "Estado 2: Verde");
+      }
+      break;
 
-  case GREEN_TO_YELLOW:
-    if (tdelta >= 2000)
-    {
-      changeState(RED, "Estado 0: Rojo");
-    }
-    break;
+    case GREEN:
+      if (vlS1 || vlS2) {
+        tini = millis();
+        break;
+      }
 
-  default:
-    break;
+      if (tdelta >= 6000 && !vlS1 && !vlS2) {
+        changeState(GREEN_TO_YELLOW, "Estado 3: Verde a Amarillo");
+      }
+      break;
+
+    case GREEN_TO_YELLOW:
+      if (tdelta >= 2000) {
+        changeState(RED, "Estado 0: Rojo");
+      }
+      break;
+
+    default:
+      break;
   }
 }
 
-void changeState(int newState, const char *message)
-{
+void changeState(int newState, const char *message) {
   state = newState;
   tini = millis();
+
+  if (newState == PEATONAL) {
+    peatonalStartedAt = millis();
+    Serial.println("Paso peatonal activado");
+  }
   Serial.println(message);
 }
 
-void updateLights()
-{
-  switch (state)
-  {
-  case RED:
-    R1 = 0;
-    Y1 = 0;
-    V1 = 1; // Semáforo 1: Verde
-    R2 = 1;
-    Y2 = 0;
-    V2 = 0; // Semáforo 2: Rojo
-    break;
+void updateLights() {
+  switch (state) {
+    case RED:
+      R1 = 0;
+      Y1 = 0;
+      V1 = 1;  // Semáforo 1: Verde
+      R2 = 1;
+      Y2 = 0;
+      V2 = 0;  // Semáforo 2: Rojo
+      break;
 
-  case RED_TO_YELLOW:
-    R1 = 0;
-    Y1 = 1;
-    V1 = 0;
-    R2 = 0;
-    Y2 = 1;
-    V2 = 0;
-    break;
+    case RED_TO_YELLOW:
+      R1 = 0;
+      Y1 = 1;
+      V1 = 0;
+      R2 = 1;
+      Y2 = 0;
+      V2 = 0;
+      break;
 
-  case GREEN:
-    R1 = 1;
-    Y1 = 0;
-    V1 = 0;
-    R2 = 0;
-    Y2 = 0;
-    V2 = 1;
-    break;
+    case GREEN:
+      R1 = 1;
+      Y1 = 0;
+      V1 = 0;
+      R2 = 0;
+      Y2 = 0;
+      V2 = 1;
+      break;
 
-  case GREEN_TO_YELLOW:
-    R1 = 0;
-    Y1 = 1;
-    V1 = 0;
-    R2 = 0;
-    Y2 = 1;
-    V2 = 0;
-    break;
+    case GREEN_TO_YELLOW:
+      R1 = 1;
+      Y1 = 0;
+      V1 = 0;
+      R2 = 0;
+      Y2 = 1;
+      V2 = 0;
+      break;
 
-  case PEATONAL:
-    R1 = 1;
-    Y1 = 0;
-    V1 = 0; // Ambos en rojo
-    R2 = 1;
-    Y2 = 0;
-    V2 = 0;
+    case PEATONAL:
+      R1 = 1;
+      Y1 = 0;
+      V1 = 0;  // Ambos en rojo
+      R2 = 1;
+      Y2 = 0;
+      V2 = 0;
 
-    if (peatonalStartedAt == 0)
-    {
-      peatonalStartedAt = millis();
-      Serial.println("Paso peatonal activado");
-    }
-
-    if (millis() - peatonalStartedAt >= 5000)
-    {
-      peatonalStartedAt = 0;
-      changeState(GREEN_TO_YELLOW, "Fin paso peatonal");
-    }
-    break;
+      if (millis() - peatonalStartedAt >= 5000) {
+        peatonalStartedAt = 0;
+        changeState(GREEN_TO_YELLOW, "Fin paso peatonal");
+      }
+      break;
   }
 
   // Actualiza pines físicos normales
@@ -455,40 +427,31 @@ void updateLights()
   digitalWrite(GREEN_LED_PIN_2, V2);
 }
 
-void read_lane_in_1()
-{
-  veh_c1 = 0;
-  if (digitalRead(CNY4) == LOW)
-    veh_c1++;
-  if (digitalRead(CNY5) == LOW)
-    veh_c1++;
-  if (digitalRead(CNY6) == LOW)
-    veh_c1++;
+int read_lane_in_1() {
+  int count = 0;
+  if (digitalRead(CNY4) == LOW) count++;
+  if (digitalRead(CNY5) == LOW) count++;
+  if (digitalRead(CNY6) == LOW) count++;
+  return count;
 }
 
-void read_lane_in_2()
-{
-  veh_c2 = 0;
-  if (digitalRead(CNY1) == LOW)
-    veh_c2++;
-  if (digitalRead(CNY2) == LOW)
-    veh_c2++;
-  if (digitalRead(CNY3) == LOW)
-    veh_c2++;
+int read_lane_in_2() {
+  int count = 0;
+  if (digitalRead(CNY1) == LOW) count++;
+  if (digitalRead(CNY2) == LOW) count++;
+  if (digitalRead(CNY3) == LOW) count++;
+  return count;
 }
 
-void pedestrian_crossing()
-{
-  if ((digitalRead(BTN_PEATONAL) == LOW) && !peatonalRequested)
-  {
+void pedestrian_crossing() {
+  if ((digitalRead(BTN_PEATONAL_S1) == HIGH || digitalRead(BTN_PEATONAL_S2) == HIGH) && !peatonalRequested) {
     peatonalRequested = true;
-    peatonalRequestTime = millis(); // Marca el momento de la solicitud
+    peatonalRequestTime = millis();  // Marca el momento de la solicitud
     Serial.println("Solicitud de paso peatonal recibida desde alguno de los semáforos.");
   }
 }
 
-void allOff()
-{
+void allOff() {
 
   // Semaforo 1
   digitalWrite(RED_LED_PIN_1, LOW);
@@ -501,32 +464,24 @@ void allOff()
   digitalWrite(GREEN_LED_PIN_2, LOW);
 }
 
-void yellow_flashing_both()
-{
+void yellow_flashing_both() {
 
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval)
-  {
+  if (currentMillis - previousMillisFlashing >= interval) {
     // save the last time you blinked the LED
-    previousMillis = currentMillis;
+    previousMillisFlashing = currentMillis;
 
     // if the LED is off turn it on and vice-versa:
-    if (YELLOW_LED_PIN_1_STATE == LOW)
-    {
+    if (YELLOW_LED_PIN_1_STATE == LOW) {
       YELLOW_LED_PIN_1_STATE = HIGH;
-    }
-    else
-    {
+    } else {
       YELLOW_LED_PIN_1_STATE = LOW;
     }
 
-    if (YELLOW_LED_PIN_2_STATE == LOW)
-    {
+    if (YELLOW_LED_PIN_2_STATE == LOW) {
       YELLOW_LED_PIN_2_STATE = HIGH;
-    }
-    else
-    {
+    } else {
       YELLOW_LED_PIN_2_STATE = LOW;
     }
 
@@ -535,13 +490,11 @@ void yellow_flashing_both()
   }
 }
 
-void connection()
-{
+void connection() {
   // Wifi connection
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
   }
@@ -554,50 +507,45 @@ void connection()
 }
 
 // Handle incoming WebSocket messages
-void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
-{
-  switch (type)
-  {
-  case WStype_CONNECTED:
-    Serial.println("Connected to WebSocket server");
-    break;
-  case WStype_DISCONNECTED:
-    Serial.println("Disconnected from WebSocket server");
-    break;
-  case WStype_TEXT:
-  {
-    Serial.printf("Received: %s\n", payload);
-    // Parse the incoming JSON message
-    StaticJsonDocument<200> doc;
-    DeserializationError error = deserializeJson(doc, payload);
-    if (error)
-    {
-      Serial.print("JSON parsing failed: ");
-      Serial.println(error.c_str());
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
+  switch (type) {
+    case WStype_CONNECTED:
+      Serial.println("Connected to WebSocket server");
       break;
-    }
-    const char *msg = doc["msg"];
-    sensor_other_city = int(doc["sensor"]);
+    case WStype_DISCONNECTED:
+      Serial.println("Disconnected from WebSocket server");
+      break;
+    case WStype_TEXT:
+      {
+        Serial.printf("Received: %s\n", payload);
+        // Parse the incoming JSON message
+        StaticJsonDocument<200> doc;
+        DeserializationError error = deserializeJson(doc, payload);
+        if (error) {
+          Serial.print("JSON parsing failed: ");
+          Serial.println(error.c_str());
+          break;
+        }
+        const char *msg = doc["msg"];
+        sensor_other_city = int(doc["sensor"]);
 
-    Serial.println(sensor_other_city);
+        Serial.println(sensor_other_city);
 
-    Serial.print("msg: ");
-    Serial.println(msg);
-  }
-  break;
-  default:
-    break;
+        Serial.print("msg: ");
+        Serial.println(msg);
+      }
+      break;
+    default:
+      break;
   }
 }
 
-void sendSignalWebsocket()
-{
+void sendSignalWebsocket() {
   webSocket.loop();
 
   unsigned long now = millis();
-  if (now - previousMillis > 2000 && webSocket.isConnected())
-  {
-    previousMillis = now;
+  if (now - previousMillisSocket > 2000 && webSocket.isConnected()) {
+    previousMillisSocket = now;
     int sensorValue = analogRead(SENSOR_PIN_S2);
 
     // Optional: use JSON format
@@ -608,6 +556,6 @@ void sendSignalWebsocket()
     String json;
     serializeJson(doc, json);
 
-    webSocket.sendTXT(json); // send sensor reading
+    webSocket.sendTXT(json);  // send sensor reading
   }
 }
